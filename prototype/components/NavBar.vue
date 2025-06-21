860fe72ff41125d1e260b6b052d156ceee707981
James Binks <james.binks@buhlergroup.com>
2025-06-22 00:12:13 +0200

    added Language dropdown to Prototype
    Signed-off-by: James Binks <james.binks@buhlergroup.com>

--- prototype/components/NavBar.vue
+++ prototype/components/NavBar.vue
@@ -1,29 +1,81 @@
 <template>
-	<div class="navbar bg-base-100 shadow-sm">
-		<div class="flex">
-			<NuxtLink to="/" class="btn btn-ghost text-xl">Logo</NuxtLink>
-		</div>
-		<div class="flex gap-2 grow w-0">
-			<div class="breadcrumbs text-sm">
-				<ul>
-					<li v-for="pathPart of pageStore.path">
-						<NuxtLink v-if="!pathPart.current" :to="pathPart.path">
-							{{ pathPart.name }}
-						</NuxtLink>
-						<span v-else>{{ pathPart.name }}</span>
-					</li>
-				</ul>
-			</div>
-		</div>
-		<MenuIcon class="z-50" v-model:menu-open="menuOpen" />
-	</div>
+  <div class="navbar bg-base-100 shadow-sm">
+    <div class="flex">
+      <NuxtLink to="/" class="btn btn-ghost text-xl">Logo</NuxtLink>
+    </div>
+
+    <div class="flex gap-2 grow w-0">
+      <div class="breadcrumbs text-sm" aria-label="Breadcrumb">
+        <ul>
+          <li v-for="pathPart in pageStore.path" :key="pathPart.path">
+            <NuxtLink v-if="!pathPart.current" :to="pathPart.path">
+              {{ pathPart.name }}
+            </NuxtLink>
+            <span v-else>{{ pathPart.name }}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <!-- Language Dropdown -->
+    <div class="relative inline-block text-left">
+      <button
+        @click="dropdownOpen = !dropdownOpen"
+        class="btn btn-outline"
+      >
+        {{ selectedOption }}
+        <svg
+          class="w-4 h-4 ml-2 inline"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M19 9l-7 7-7-7"
+          />
+        </svg>
+      </button>
+
+      <div
+        v-if="dropdownOpen"
+        class="absolute right-0 mt-2 w-28 bg-white border rounded shadow-lg z-50"
+      >
+        <ul>
+          <li
+            v-for="option in options"
+            :key="option"
+            @click="selectOption(option)"
+            class="px-4 py-2 hover:bg-gray-100 cursor-pointer"
+          >
+            {{ option }}
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <!-- Menu Icon -->
+    <MenuIcon class="z-50 ml-2" v-model:menu-open="menuOpen" />
+  </div>
 </template>
 
-<script lang="ts" setup>
+<script setup lang="ts">
+import { ref } from 'vue';
+
 const pageStore = usePageStore();
 const menuOpen = defineModel<boolean | undefined>("menu-open", {
-	required: true,
+  required: true,
 });
-</script>
 
-<style></style>
+// Dropdown state
+const dropdownOpen = ref(false);
+const options = ['EN', 'DE', 'IT', 'FR'];
+const selectedOption = ref('DE');
+
+function selectOption(option: string) {
+  selectedOption.value = option;
+  dropdownOpen.value = false;
+}
+</script>
